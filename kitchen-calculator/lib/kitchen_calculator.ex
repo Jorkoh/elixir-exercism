dca4baf5e11e9bacd6e808c451b62c63fe2066b4
Jorkoh <kohru95@gmail.com>
2023-09-10 02:14:11 +0200

    kitchen-calculator exercise

---
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -0,0 +1,19 @@
+defmodule KitchenCalculator do
+  def get_volume({_, volume}), do: volume
+
+  def to_milliliter({:milliliter, volume}), do: {:milliliter, volume}
+  def to_milliliter({:cup, volume}), do: {:milliliter, volume * 240}
+  def to_milliliter({:fluid_ounce, volume}), do: {:milliliter, volume * 30}
+  def to_milliliter({:teaspoon, volume}), do: {:milliliter, volume * 5}
+  def to_milliliter({:tablespoon, volume}), do: {:milliliter, volume * 15}
+
+  def from_milliliter({:milliliter, volume}, :milliliter = unit), do: {unit, volume}
+  def from_milliliter({:milliliter, volume}, :cup = unit), do: {unit, volume / 240}
+  def from_milliliter({:milliliter, volume}, :fluid_ounce = unit), do: {unit, volume / 30}
+  def from_milliliter({:milliliter, volume}, :teaspoon = unit), do: {unit, volume / 5}
+  def from_milliliter({:milliliter, volume}, :tablespoon = unit), do: {unit, volume / 15}
+
+  def convert(volume_pair, unit) do
+    to_milliliter(volume_pair) |> from_milliliter(unit)
+  end
+end
